64cad9f3974e601594528ef64e3058d2c29a179c
emmageo3 <kanfanyemma22@gmail.com>
2022-03-06 18:07:45 +0000

    bootstrap

--- Views/User/LoggedIn.php
+++ Views/User/LoggedIn.php
@@ -2,11 +2,40 @@
 <html>
 <head>
 	<meta charset="utf8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<title>Logged In</title>
     <link type="text/css" href="<?php echo BASE_URL ?>/Views/Assets/style.css" rel="stylesheet" />
 </head>
 
 <body>
-    <p class="message_box">Howdy <b><?php echo $_SESSION['username']; ?></b>, you logged in! <a href="<?php echo BASE_URL; ?>/User/Logout">Logout!</a></p>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">senRecensement</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Voir les organisations</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Ajouter une organisation</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="<?php echo BASE_URL; ?>/User/Logout">Se déconnecter!</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+
+    <p class="message_box">Bienvenue <b><?php echo $_SESSION['username']; ?></b>, </p>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
--- Views/User/LoginForm.php
+++ Views/User/LoginForm.php
@@ -2,11 +2,28 @@
 <html>
 <head>
 	<meta charset="utf8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<title>Login Form</title>
     <link type="text/css" href="<?php echo BASE_URL ?>/Views/Assets/style.css" rel="stylesheet" />
 </head>
 
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">senRecensement</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="<?php echo BASE_URL ?>/User/Register">S'inscrire</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
     <div class="wrapper">
         <?php if( isset( $_SESSION['__message'] ) && $_SESSION['__message'] != '' ) : ?>
             <div class="message_box">
@@ -28,10 +45,11 @@
             <div class="btn-group">
                 <input type="submit" name="submit" class="btn-primary" value="Se connecter" />
                 <input type="reset" name="reset" value="Annuler" />
-                <a class="register-btn" href="<?php echo BASE_URL ?>/User/Register">S'inscrire ici </a>
             </div>
             
         </form>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
