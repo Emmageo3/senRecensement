fc66ab4fd79b89089c5c2cbd6072c552834e98ec
emmageo3 <kanfanyemma22@gmail.com>
2022-03-09 13:37:30 +0000

    pushh

--- Models/OrgModel.php
+++ Models/OrgModel.php
@@ -1,48 +1,9 @@
 <?php
 
-class OrgModel{
-    public $conn;
-    
-    public function __construct(){
-        $this->conn = mysqli_connect( 'localhost', 'root', '', 'mvc' ) or die ( 'Could not connect to server' );
-        
-    }
-    
-    public function Addnew( $data ){
-        foreach( $data as $value ){
-            if( $value === '' ){
-                return false;
-            }
+    class OrgModel{
+        public $conn;
+
+        public function __construct(){
+            $this->conn = mysqli_connect('localhost', 'root', '', 'mvc');
         }
-        extract( $data );
-        $password = md5( $password );
-        $sql = "INSERT INTO `mvc`.`user` (`ID`, `username`, `pass`, `fullname`) VALUES (NULL, '{$username}', '{$password}', '{$fullname}')";
-        
-        $query = mysqli_query( $this->conn, $sql );
-        if( mysqli_affected_rows( $this->conn ) > 0 ){
-            return true;
-        }
-        return false;
     }
-    
-    public function is_user_exists( $user_name, $password ){
-        $password = md5( $password );
-        $sql = "SELECT * FROM `user` WHERE `username` = '{$user_name}' AND `pass` = '{$password}'";
-        
-        $query = mysqli_query( $this->conn, $sql );
-        if( mysqli_num_rows( $query ) > 0 ){
-            return true;
-        }
-        return false;
-    }
-    
-    public function is_username_exists( $user_name ){
-        $sql = "SELECT * FROM `user` WHERE `username` = '{$user_name}'";
-        $query = mysqli_query( $this->conn, $sql );
-        if( mysqli_num_rows( $query ) > 0 ){
-            return true;
-        }
-        return false;
-    }
-    
-}
--- Views/User/LoggedIn.php
+++ Views/User/LoggedIn.php
@@ -36,6 +36,179 @@
 
     <p class="message_box">Bienvenue <b><?php echo $_SESSION['username']; ?></b>, </p>
 
+    <div class="org-form wrapper">
+        <div class="title">
+            <h1 class="text-center mt-4">Formulaire d'enregistrement d'une organisation</h1>
+        </div>
+
+        <form action="<?php echo BASE_URL ?>/User/ProcessLogin" method="POST" >
+        
+            <div class="form-group">
+                <label>Nom de l'organisation</label>
+                <input type="text" name="username" value="" required="required" />
+            </div>
+            
+            <div class="form-group">
+                <label>Nom complet du répondant</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>Adresse e-mail du répondant</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>Numéro de téléphone du répondant</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>Siége social de l'organisation</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>Date de création de l'organisation</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>Numéro de l'organisation sur le registre de commerce</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>Ninea</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>secteur d'activité de l'organisation</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>Page web de l'organisation</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>Nombre d'employés de l'organisation</label>
+                <input type="password" name="password" value="" required="required" />
+            </div>
+
+            <div class="form-group">
+                <label>Les employés de l'organisation sont-ils soumis a des contrats formels ?</label>
+                <select name="reponse" id="reponse">
+                    <option>oui</option>
+                    <option>non</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label>L'organisation, prend-elle en compte les cotisations sociales et patronales ?</label>
+                <select name="reponse" id="reponse">
+                    <option>oui</option>
+                    <option>non</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label>Existe-t-il un organigramme clair et respecté au sein de l'organisation ?</label>
+                <select name="reponse" id="reponse">
+                    <option>oui</option>
+                    <option>non</option>
+                </select>
+            </div>
+            
+            
+
+            <div class="form-group">
+                <label>Quartier</label>
+                <?php 
+
+                    $mysqli = new MySQLi('localhost', 'root', '', 'mvc');
+
+                    $resultSet = $mysqli->query("SELECT libelle FROM  quartier");
+
+                ?>
+
+                <select name="quartier">
+
+                    <?php
+
+                        while ($rows = $resultSet->fetch_assoc())
+                        {
+                            $libelle = $rows['libelle'];
+                            echo "<option value='$libelle'>$libelle</option>";
+                        }
+
+                    ?>
+
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label>Statut juridique</label>
+                <?php 
+
+                    $mysqli = new MySQLi('localhost', 'root', '', 'mvc');
+
+                    $resultSet = $mysqli->query("SELECT libelle FROM  statut_juridique");
+
+                ?>
+
+                <select name="statut_juridique">
+
+                    <?php
+
+                        while ($rows = $resultSet->fetch_assoc())
+                        {
+                            $libelle = $rows['libelle'];
+                            echo "<option value='$libelle'>$libelle</option>";
+                        }
+
+                    ?>
+
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label>Statut juridique</label>
+                <?php 
+
+                    $mysqli = new MySQLi('localhost', 'root', '', 'mvc');
+
+                    $resultSet = $mysqli->query("SELECT libelle FROM  statut_juridique");
+
+                ?>
+
+                <select name="statut_juridique">
+
+                    <?php
+
+                        while ($rows = $resultSet->fetch_assoc())
+                        {
+                            $libelle = $rows['libelle'];
+                            echo "<option value='$libelle'>$libelle</option>";
+                        }
+
+                    ?>
+
+                </select>
+            </div>  
+
+            <div class="btn-group">
+                <input type="submit" name="submit" class="btn-primary" value="Se connecter" />
+                <input type="reset" name="reset" value="Annuler" />
+            </div>
+            
+            
+        </form>
+
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
